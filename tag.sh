35df3830ed05d3148d126de3149c0ca0931696dc
89leon@gmail.com <89leon@gmail.com>
2019-10-30 16:52:59 +0200

    a

--- tag.sh
+++ tag.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [[ ${GIT_BRANCH} == "master" ]]; then
     echo 'master'
-    sudo docker tag gcr.io/echo123/echoapp:latest echoapp:0.1."${BUILD_NUMBER}"
+    docker tag gcr.io/echo123/echoapp:latest echoapp:0.1."${BUILD_NUMBER}"
 fi
 if [[ ${GIT_BRANCH} == "dev" ]]; then
-    sudo docker tag gcr.io/echo123/echoapp:latest echoapp:dev-"${GIT_COMMIT}"
+    docker tag gcr.io/echo123/echoapp:latest echoapp:dev-"${GIT_COMMIT}"
 fi
 if [[ ${GIT_BRANCH} == "staging" ]]; then
-    sudo docker tag gcr.io/echo123/echoapp:latest echoapp:staging-"${GIT_COMMIT}"
+    docker tag gcr.io/echo123/echoapp:latest echoapp:staging-"${GIT_COMMIT}"
 fi
