e90478eb933f3e8419459f838faa904b70c8d56f
89leon@gmail.com <89leon@gmail.com>
2019-10-30 16:40:32 +0200

    a

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ ${GIT_BRANCH} == "master" ]]; then
-   gcloud docker push gcr.io/echo123/echoapp:0.1."${BUILD_NUMBER}"
+   $GCLOUD_PATH/gcloud docker push gcr.io/echo123/echoapp:0.1."${BUILD_NUMBER}"
 fi
 if [[ ${GIT_BRANCH} == "dev" ]]; then
-   gcloud docker push gcr.io/echo123/echoapp:dev-"${GIT_COMMIT}"
+   $GCLOUD_PATH/gcloud docker push gcr.io/echo123/echoapp:dev-"${GIT_COMMIT}"
 fi
 if [[ ${GIT_BRANCH} == "staging" ]]; then
-   gcloud docker push gcr.io/echo123/echoapp:staging-"${GIT_COMMIT}"
+   $GCLOUD_PATH/gcloud docker push gcr.io/echo123/echoapp:staging-"${GIT_COMMIT}"
 fi
